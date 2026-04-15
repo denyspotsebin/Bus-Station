9ab3f583c20bfad3735147e07648c51777a22cd9
Potsebin Denis <denys.potsebin@nure.ua>
2026-04-15 20:34:17 +0300

    Initial commit : стоворення базових моделей "Station", "Trip"

---
+++ Bus-Station/Models/Station.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bus_Station.Models
+{
+    public class Station
+    {
+        public string Name { get; set; }
+        public int MinutesFromStart { get; set; }
+
+        public override string ToString() => Name;
+    }
+}
---
+++ Bus-Station/Models/Trip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bus_Station.Models
+{
+    public class Trip
+    {
+
+        public string TripNumber { get; set; }
+        public string FinalDestination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int TotalSeats { get; set; }
+        public int SoldSeats { get; set; }
+
+        public List<Station> IntermediateStations { get; set; } = new List<Station>();
+
+        public int FreeSeats => TotalSeats - SoldSeats;
+        public bool HasFreeSeats => FreeSeats > 0;
+    }
+}
